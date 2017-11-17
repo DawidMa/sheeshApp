2ef3f2f4b785063ee83c9729ade2bb3103df1d51
DawidMa <dmaruszczyk@yahoo.de>
2017-11-17 12:09:44 +0100

    Added gif

--- code/app/src/main/java/de/dhkarlsruhe/it/sheeshapp/sheeshapp/WelcomeActivity.java
+++ code/app/src/main/java/de/dhkarlsruhe/it/sheeshapp/sheeshapp/WelcomeActivity.java
@@ -1,13 +1,19 @@
 package de.dhkarlsruhe.it.sheeshapp.sheeshapp;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 /**
  * Created by Informatik on 17.11.2017.
@@ -15,20 +21,18 @@ import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    ImageView image;
+    GlideDrawableImageViewTarget imageViewTarget;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-       /* Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        }, 4000);
-*/
+        image = findViewById(R.id.imgWelLogo);
+        if (image != null)
+            imageViewTarget = new GlideDrawableImageViewTarget(image);
+            Glide.with(this).load(R.drawable.giphy).into(imageViewTarget);
     }
 
     public void openTestMode(View view) {
