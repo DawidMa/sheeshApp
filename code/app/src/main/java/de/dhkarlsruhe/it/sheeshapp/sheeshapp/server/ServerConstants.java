18ffd83097b83cf7c18f2b68681021fb2c1dd549
Dawid <dmaruszczyk@yahoo.de>
2018-05-04 23:03:42 +0200

    signup online
    works online without much error handling. Response printed out as toast with

---
+++ code/app/src/main/java/de/dhkarlsruhe/it/sheeshapp/sheeshapp/server/ServerConstants.java
@@ -0,0 +1,11 @@
+package de.dhkarlsruhe.it.sheeshapp.sheeshapp.server;
+
+/**
+ * Created by d0272129 on 04.05.18.
+ */
+
+public class ServerConstants {
+
+    public static final String URL = "http://sheeshapp.it.dh-karlsruhe.de:8080/server-0.0.1-SNAPSHOT/";
+
+}
