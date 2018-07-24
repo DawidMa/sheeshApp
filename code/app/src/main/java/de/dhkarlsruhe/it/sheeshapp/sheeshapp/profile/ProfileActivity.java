6d458e10c4a4c15f75c9d477af78fb8412cbf889
Dawid <dmaruszczyk@yahoo.de>
2018-07-24 14:30:04 +0200

    Profile of friend
    - sending profileimage of user to profileactivity
    - getting profile infos online
    - added userprofileobject
    - new icons and design of profile

--- code/app/src/main/java/de/dhkarlsruhe/it/sheeshapp/sheeshapp/profile/ProfileActivity.java
+++ code/app/src/main/java/de/dhkarlsruhe/it/sheeshapp/sheeshapp/profile/ProfileActivity.java
@@ -1,9 +1,16 @@
 package de.dhkarlsruhe.it.sheeshapp.sheeshapp.profile;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,9 +19,17 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
+import de.dhkarlsruhe.it.sheeshapp.sheeshapp.MyAlert;
 import de.dhkarlsruhe.it.sheeshapp.sheeshapp.R;
 import de.dhkarlsruhe.it.sheeshapp.sheeshapp.SharedPrefConstants;
+import de.dhkarlsruhe.it.sheeshapp.sheeshapp.friend.Friend;
+import de.dhkarlsruhe.it.sheeshapp.sheeshapp.server.FriendRequestObject;
 import de.dhkarlsruhe.it.sheeshapp.sheeshapp.server.ServerConstants;
 
 /**
@@ -23,14 +38,15 @@ import de.dhkarlsruhe.it.sheeshapp.sheeshapp.server.ServerConstants;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView tvName;
-    private TextView tvSessions;
-    private TextView tvTobacco;
-    private Profile profile;
+    private TextView tvName, tvNumberShishas, tvTobacco, tvSince, tvNumberFriends;
+    private String name, numberShishas, tobacco, since, numberFriends;
+    private UserProfileObject userProfileObject;
+    private ImageView profileImage;
+    private Window window;
+    private long friendid;
 
-    private String name;
-    private long sessions;
-    private String tobacco;
+    private Profile profile;
+    private Friend friend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +58,27 @@ public class ProfileActivity extends AppCompatActivity {
 
     private void init() {
         profile = new Profile(this);
+        friend = new Friend(this);
         tvName = findViewById(R.id.tvProName);
-        tvSessions = findViewById(R.id.tvProSessions);
+        tvNumberShishas = findViewById(R.id.tvProShishas);
         tvTobacco = findViewById(R.id.tvProTobacco);
+        tvSince = findViewById(R.id.tvProSince);
+        tvNumberFriends = findViewById(R.id.tvProFriends);
+        profileImage = findViewById(R.id.imgProImage);
+        window = getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.colorAccent));
+
 
         name = profile.getProfileName();
-        sessions = profile.getTotalSessions();
+        tvName.setText(name);
+
+        Bundle extras = getIntent().getExtras();
+        byte[] b = extras.getByteArray("PROFILE_IMAGE");
+        friendid = extras.getLong("FRIEND_ID");
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(b, 0, b.length);
+
+        profileImage.setImageBitmap(bmp);
     }
 
     public void onlineLoader() {
@@ -56,8 +87,8 @@ public class ProfileActivity extends AppCompatActivity {
         final StringRequest request = new StringRequest(ServerConstants.URL+"user/info?user_id="+user_id, new Response.Listener<String>() {
             @Override
             public void onResponse(String string) {
-                tobacco = string;
-                makeProfile();
+                parseData(string);
+                reloadProfile();
             }
         }, new Response.ErrorListener() {
             @Override
@@ -69,14 +100,48 @@ public class ProfileActivity extends AppCompatActivity {
         rQueue.add(request);
     }
 
+    private void parseData(String string) {
+        Gson json = new Gson();
+        try {
+            userProfileObject = json.fromJson(string,UserProfileObject.class);
+        } catch (Exception e) {
+            userProfileObject.setNumberFriends("#111");
+            userProfileObject.setNumberShishas("555");
+            userProfileObject.setSince("12.12.2012");
+            userProfileObject.setTobacco("Dawid's Tabak");
+        }
+    }
+
     private void errorResponse() {
         Toast.makeText(this,"Error loading profile", Toast.LENGTH_SHORT).show();
         this.finish();
     }
 
-    private void makeProfile() {
-        tvName.setText(name);
-        tvSessions.setText(sessions+"");
-        tvTobacco.setText(tobacco);
+    private void reloadProfile() {
+        tvTobacco.setText(userProfileObject.getTobacco());
+        tvSince.setText(userProfileObject.getSince());
+        tvNumberFriends.setText(userProfileObject.getNumberFriends());
+        tvNumberShishas.setText(userProfileObject.getNumberShishas());
+    }
+
+    public void deleteFriend(View view) {
+        MyAlert alert = new MyAlert(this, "Löschen", "Bist du sicher dass du " + tvName.getText().toString() + " löschen willst?");
+        alert.setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try {
+                    friend.deleteFriendOnline(friendid);
+                } catch (Exception e) {
+
+                }
+            }
+        });
+        alert.show();
     }
 }
---
+++ code/app/src/main/java/de/dhkarlsruhe/it/sheeshapp/sheeshapp/profile/UserProfileObject.java
@@ -0,0 +1,45 @@
+package de.dhkarlsruhe.it.sheeshapp.sheeshapp.profile;
+
+/**
+ * Created by d0272129 on 18.07.18.
+ */
+
+public class UserProfileObject {
+
+    private String tobacco;
+    private String numberShishas;
+    private String since;
+    private String numberFriends;
+
+    public String getTobacco() {
+        return tobacco;
+    }
+
+    public void setTobacco(String tobacco) {
+        this.tobacco = tobacco;
+    }
+
+    public String getNumberShishas() {
+        return numberShishas;
+    }
+
+    public void setNumberShishas(String numberShishas) {
+        this.numberShishas = numberShishas;
+    }
+
+    public String getSince() {
+        return since;
+    }
+
+    public void setSince(String since) {
+        this.since = since;
+    }
+
+    public String getNumberFriends() {
+        return numberFriends;
+    }
+
+    public void setNumberFriends(String numberFriends) {
+        this.numberFriends = numberFriends;
+    }
+}
